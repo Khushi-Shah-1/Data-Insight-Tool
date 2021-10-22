a94049694769539e2bda2dcb4a0e497369d78943
Khushi-Shah-1 <67700189+Khushi-Shah-1@users.noreply.github.com>
2021-10-22 19:26:37 +0530

    Update MissingValue.py

--- MissingValue.py
+++ MissingValue.py
@@ -64,9 +64,9 @@ def app():
       #  towrite = io.BytesIO()
        # downloaded_file = df.to_csv(towrite, encoding='utf-8', index=False, header=True)
        # towrite.seek(0)  # reset pointer
-       # b64 = base64.b64encode(towrite.read()).decode()  # some strings
-       # linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="anomaly.csv">Download csv file</a>'
-        linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{file1}" download="anomaly.csv">Download csv file</a>'
+        b64 = base64.b64encode(towrite.read()).decode()  # some strings
+        linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="anomaly.csv">Download csv file</a>'
+        #linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{file1}" download="anomaly.csv">Download csv file</a>'
         st.markdown(linko, unsafe_allow_html=True)
         
         #from urllib import request
